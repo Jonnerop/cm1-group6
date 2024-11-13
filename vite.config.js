7f99302c8b8e332bca7e6e3a8a2a9acb4ee5fc34
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2024-11-13 15:45:03 +0200

    changes deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/coding-marathon1-group6/',
+  base: '/cm1-group6/',
   plugins: [react()],
   server: {
     port: 3002,
