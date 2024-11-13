801ff14955e1689195d77afdeba35c5670b98654
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2024-11-13 15:38:12 +0200

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css';
+function Home() {
+    return (
+      <div className="home-div">
+        <h2>Group 6</h2>
+        <p>Code Marathon 1</p>
+      </div>
+    );
+  }
+  
+  export default Home;
